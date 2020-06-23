608d6dbd8cb1ea943d84efa4b91268ab0208014a
junheechoi <junidream@gmail.com>
2020-06-23 23:49:50 +0900

    Initial Commit

---
+++ ViewNavigationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewNavigationTest
+//
+//  Created by JunHee Choi on 2020/06/23.
+//  Copyright © 2020 JunHee Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
